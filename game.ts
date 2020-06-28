82189b1cd9bdb288139eadb25115516126f6534b
James Choca <jchoca@repay.io>
2020-06-28 15:41:44 -0700

    label updates

--- game.ts
+++ game.ts
@@ -8,13 +8,13 @@ class Game {
 
     private readonly _orbitRadius: number;
 
-    constructor(canvasElement : string) {
+    constructor(canvasElement: string) {
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
         this._engine = new BABYLON.Engine(this._canvas, true);
         this._orbitRadius = 598;
     }
 
-    createScene() : void {
+    createScene(): void {
         this._scene = new BABYLON.Scene(this._engine);
         this._scene.clearColor = new BABYLON.Color4(150, 150, 150);
 
@@ -23,11 +23,15 @@ class Game {
         let points = this.createAnimation(earth);
         earth.position = points[0];
 
-        const camera = new BABYLON.ArcRotateCamera('camera1', 0, 0, 0, new BABYLON.Vector3(this._orbitRadius, 0, this._orbitRadius), this._scene);
+        let earthLabel = this.makePlanetLabel("EARTH", "black", 100);
+        earthLabel.parent = earth;
+
+        const camera = new BABYLON.ArcRotateCamera('camera1', Math.PI, 0, 0,
+            new BABYLON.Vector3(this._orbitRadius, 0, -this._orbitRadius), this._scene);
         camera.setTarget(earth);
         camera.attachControl(this._canvas, false);
 
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,0,500), this._scene);
+        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 0, 500), this._scene);
         this._light.intensity = 0.5;
 
         let sun = BABYLON.MeshBuilder.CreateSphere("sun",
@@ -45,7 +49,7 @@ class Game {
         });
     }
 
-    createAnimation(mesh : BABYLON.Mesh) {
+    createAnimation(mesh: BABYLON.Mesh) {
         const animation = new BABYLON.Animation("myAnimation", "position", 10, BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
             BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
 
@@ -73,26 +77,38 @@ class Game {
         return [keys, points];
     }
 
-    showAxis(size: number) {
-        const makeTextPlane = (text, color, size) => {
-            const dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, this._scene, true);
-            dynamicTexture.hasAlpha = true;
-            dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
-            const plane = new (BABYLON.Mesh as any).CreatePlane("TextPlane", size, this._scene, true);
-            plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", this._scene);
-            plane.material.backFaceCulling = false;
-            plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
-            plane.material.diffuseTexture = dynamicTexture;
-            return plane;
-        };
+    makePlanetLabel(text: string, color: string, size: number) : BABYLON.Mesh {
+        const dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 200, this._scene, true);
+        dynamicTexture.hasAlpha = true;
+        dynamicTexture.drawText(text, 0, 200, "bold 36px Arial", color, "transparent", true);
+        const plane = new (BABYLON.Mesh as any).CreatePlane("TextPlane", size, this._scene, true);
+        plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", this._scene);
+        plane.material.backFaceCulling = false;
+        plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+        plane.material.diffuseTexture = dynamicTexture;
+        return plane as BABYLON.Mesh;
+    }
 
+    makeAxisLabel(text: string, color: string, size: number) : BABYLON.Mesh {
+        const dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 40, this._scene, true);
+        dynamicTexture.hasAlpha = true;
+        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+        const plane = new (BABYLON.Mesh as any).CreatePlane("TextPlane", size, this._scene, true);
+        plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", this._scene);
+        plane.material.backFaceCulling = false;
+        plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+        plane.material.diffuseTexture = dynamicTexture;
+        return plane as BABYLON.Mesh;
+    }
+
+    showAxis(size: number) {
         const axisX = BABYLON.Mesh.CreateLines("axisX", [
             BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
             new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
         ], this._scene);
         axisX.color = new BABYLON.Color3(1, 0, 0);
 
-        const xChar = makeTextPlane("X", "red", size / 10);
+        const xChar = this.makeAxisLabel("X", "red", size / 10);
         xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
 
         const axisY = BABYLON.Mesh.CreateLines("axisY", [
@@ -101,7 +117,7 @@ class Game {
         ], this._scene);
         axisY.color = new BABYLON.Color3(0, 1, 0);
 
-        const yChar = makeTextPlane("Y", "green", size / 10);
+        const yChar = this.makeAxisLabel("Y", "green", size / 10);
         yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
 
         const axisZ = BABYLON.Mesh.CreateLines("axisZ", [
@@ -110,11 +126,11 @@ class Game {
         ], this._scene);
         axisZ.color = new BABYLON.Color3(0, 0, 1);
 
-        const zChar = makeTextPlane("Z", "blue", size / 10);
+        const zChar = this.makeAxisLabel("Z", "blue", size / 10);
         zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
     }
 
-    doRender() : void {
+    doRender(): void {
         this._engine.runRenderLoop(() => {
             this._scene.render();
         });
